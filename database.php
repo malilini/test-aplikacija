1f45867bfd6e2fb8c682644baba01f6bcad48c93
Sanja Krmpotic <sanja.krmpotic1@gmail.com>
2021-04-15 22:23:16 +0200

    Drugi commit

---
+++ database.php
@@ -0,0 +1,82 @@
+<?php
+
+class Database {
+
+    private $dbhost = 'remotemysql.com';
+    private $dbname = 'mGZmUr5Loc';
+    private $dbuser = 'mGZmUr5Loc';
+    private $dbpass = 'UUWN1X7a44';
+    private $connection;
+
+    public function __construct() 
+    {
+        $dsn = "mysql:host=".$this->dbhost.";dbname=".$this->dbname;
+
+        try {
+
+            $pdo = new PDO($dsn, $this->dbuser, $this->dbpass);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $this->connection = $pdo;
+
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
+
+
+    public function get_unosi() {
+
+        $unosi = [];
+
+        try {
+
+            $stmt = $this->connection->prepare("SELECT * FROM Ispis");
+            $stmt->execute();
+
+            if($stmt->rowCount()) {
+                $unosi = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            return $unosi;
+
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+
+    public function insert($params) {
+
+        try {
+            //započinjanje transakcije
+            $this->connection->beginTransaction();
+
+            $unos = intval($params['unos']);
+
+            $stmt = $this->connection->prepare(
+                "INSERT INTO Ispis (Tekst) 
+                VALUES (:unos)"
+            );
+
+            $stmt->bindParam(':unos', $unos);
+
+            $result = $stmt->execute();
+
+            //Izvršiti SQL upit
+            $this->connection->commit();
+
+        } catch(PDOException $e) {
+
+            $this->connection->rollBack();
+            throw $e;
+        }
+
+    }
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <?php
 
-    echo 'Ovo je naša druga aplikacija na produkciji!';
-    echo '<br><br>';
+include 'database.php';
 
-    echo date('d.m.Y H:i:s', time());
-    echo '<hr>';
+$db = new Database();
 
+$unosi = $db->get_unosi();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(!empty($_POST['unos'])) {
+        $db->insert($_POST);
+    }
+}
+
+echo 'Ovo je naša druga aplikacija na produkciji!';
+echo '<br><br>';
+
+echo date('d.m.Y H:i:s', time());
+echo '<hr>';
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heroku test 2</title>
+</head>
+<body>
+
+    <h3>Unos novog teksta</h3>
+
+    <div>
+    <form action="" method="POST">
+        <input type="text" id="tekst" name="tekst">
+        <br><br>
+        <input type="submit" name="unos" value="Spremi">
+    </form>
+    </div>
+
+
+    <h3> Ispis svih tekstova iz baze podataka</h3>
+    <table border="1">
+        <tr>
+            <th>#</th>
+            <th>Tekst</th>
+        </tr>
+
+        <?php if(!empty($unosi)): ?>
+            <?php foreach($unosi as $unos): ?>
+                <tr>
+                    <td><?php echo '#'; ?></td>
+                    <td><?php echo $unos['unos']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+            <?php endif; ?>
+
+    </table>
+
+</body>
+</html>
